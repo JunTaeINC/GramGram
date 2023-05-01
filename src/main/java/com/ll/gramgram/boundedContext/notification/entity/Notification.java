d939ce01fdc6412c0cb958d04fea6eeeb607f2bc
JunTaeINC <juntea3523@naver.com>
2023-05-01 10:36:42 +0900

    feat : 알림기능 구현, 알림 html 생성

---
+++ src/main/java/com/ll/gramgram/boundedContext/notification/entity/Notification.java
@@ -0,0 +1,33 @@
+package com.ll.gramgram.boundedContext.notification.entity;
+
+import com.ll.gramgram.base.baseEntity.BaseEntity;
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Notification extends BaseEntity {
+
+    private LocalDateTime readDate;
+    @ManyToOne
+    @ToString.Exclude
+    private InstaMember toInstaMember;
+    @ManyToOne
+    @ToString.Exclude
+    private InstaMember fromInstaMember;
+    private String typeCode;
+    private String oldGender;
+    private int oldAttractiveTypeCode;
+    private String newGender;
+    private int newAttractiveTypeCode;
+}
